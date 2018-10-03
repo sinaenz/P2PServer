fa1d77753124f43f117f3be3175388f98de45933
sina <eb.sina@gmail.com>
2018-10-03 16:58:53 +0330

    Ready to Test!

--- EchoServer.py
+++ EchoServer.py
@@ -1,5 +1,6 @@
 import socket
-
+import time
+from threading import Thread
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
@@ -7,19 +8,31 @@ sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 sock.bind(('0.0.0.0', 4000))
 sock.listen(10)
 
+
+def count():
+    for i in range(1024):
+        print(i)
+        time.sleep(1)
+        print(((i+1) // 10 + 1) * '\b')
+
+
 while True:
     current_connection, address = sock.accept()
     while True:
         data = str(current_connection.recv(2048), 'ascii').rstrip()
         print(data)
+        t = Thread(target=count, name='salam', daemon=True)
+        t.start()
 
         if data == 'quit' or not data:
             current_connection.shutdown(1)
+            t.join()
             current_connection.close()
             break
 
         elif data == 'stop':
             current_connection.shutdown(1)
+            t.join()
             current_connection.close()
             exit()
 
